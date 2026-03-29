4bc7b100ee4d6791f3cfe335e964f26735d4e732
Jan Krajewski <krajews.dev.@gmail.com>
2026-03-30 00:18:03 +0200

    Redirect and fetch fix

--- docs/v1.0.0/public/main.js
+++ docs/v1.0.0/public/main.js
@@ -1,13 +1,9 @@
 export default {
   start: async () => {
-    const response = await fetch('UCalc/versions.json');
+    const response = await fetch('/UCalc/versions.json');
     if (!response.ok) {
-      response = await fetch('/versions.json')
-      if(!response.ok)
-      {
-        console.warn('Could not load versions.json');
-        return;
-      }
+      console.warn('Could not load versions.json');
+      return;
     }
 
     const versions = await response.json();
